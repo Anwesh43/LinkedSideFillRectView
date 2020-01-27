0cb26061f83382dbbe02da0c32ac066f4c7bf507
Anwesh43 <anweshthecool0@gmail.com>
2020-01-27 08:41:17 +0530

    adding State class

--- sidefillsteprectview/src/main/java/com/anwesh/uiprojects/sidefillsteprectview/SideFillStepRectVew.kt
+++ sidefillsteprectview/src/main/java/com/anwesh/uiprojects/sidefillsteprectview/SideFillStepRectVew.kt
@@ -76,4 +76,24 @@ class SideFillStepRectView(ctx : Context) : View(ctx) {
         }
         return true
     }
+
+    data class Stater(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun udpate(cb : (Float) -> Unit) {
+            scale += scGap * dir
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
+    }
 }
