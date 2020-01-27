a8c26fb69b0c6361a5b12889396f75b80ae3a7d1
Anwesh43 <anweshthecool0@gmail.com>
2020-01-27 08:37:58 +0530

    adding Canvas extension methods

--- sidefillsteprectview/src/main/java/com/anwesh/uiprojects/sidefillsteprectview/SideFillStepRectVew.kt
+++ sidefillsteprectview/src/main/java/com/anwesh/uiprojects/sidefillsteprectview/SideFillStepRectVew.kt
@@ -12,6 +12,7 @@ import android.graphics.RectF
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Path
 
 val nodes : Int = 5
 val circles : Int  = 5
@@ -26,3 +27,37 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawStepFillCircle(i : Int, scale : Float, w : Float, paint : Paint) {
+    val gap : Float = w / (circles)
+    val sf : Float = scale.sinify().divideScale(i, circles)
+    val r : Float = gap / rFactor
+    save()
+    translate(gap * i + r, 0f)
+    paint.color = staticColor
+    drawCircle(0f, 0f, r, paint)
+    save()
+    paint.color = fillColor
+    val path : Path = Path()
+    path.addCircle(0f, 0f, r, Path.Direction.CW)
+    clipPath(path)
+    drawRect(RectF(-r, -r, -r + 2 * r * sf, r), paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawStepFillCircles(scale : Float, w : Float, paint : Paint) {
+    for (j in 0..(circles - 1)) {
+        drawStepFillCircle(j, scale, w, paint)
+    }
+}
+
+fun Canvas.drawSFCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val hGap : Float = h / (nodes + 1)
+    save()
+    translate(0f, hGap * (i + 1))
+    drawStepFillCircles(scale, w, paint)
+    restore()
+}
