785a38607562136159fb56bf7190aef1275332a3
Anwesh43 <anweshthecool0@gmail.com>
2020-01-27 08:39:35 +0530

    adding SideFillStepRectView class

--- sidefillsteprectview/src/main/java/com/anwesh/uiprojects/sidefillsteprectview/SideFillStepRectVew.kt
+++ sidefillsteprectview/src/main/java/com/anwesh/uiprojects/sidefillsteprectview/SideFillStepRectVew.kt
@@ -61,3 +61,19 @@ fun Canvas.drawSFCNode(i : Int, scale : Float, paint : Paint) {
     drawStepFillCircles(scale, w, paint)
     restore()
 }
+
+class SideFillStepRectView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
