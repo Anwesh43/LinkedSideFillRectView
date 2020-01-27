47d3594837a117c3b48a9f5aa716602563ad120f
Anwesh43 <anweshthecool0@gmail.com>
2020-01-27 08:50:37 +0530

    adding SideStepFillRect class

--- sidefillsteprectview/src/main/java/com/anwesh/uiprojects/sidefillsteprectview/SideFillStepRectVew.kt
+++ sidefillsteprectview/src/main/java/com/anwesh/uiprojects/sidefillsteprectview/SideFillStepRectVew.kt
@@ -166,4 +166,28 @@ class SideFillStepRectView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SideStepFillCircle(var i : Int) {
+
+        private val root : SFCNode = SFCNode(0)
+        private var curr : SFCNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
