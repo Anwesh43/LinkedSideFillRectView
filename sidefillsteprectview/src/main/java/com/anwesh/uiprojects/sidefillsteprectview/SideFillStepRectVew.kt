8f998c5320c0f73d7e34bee84830a15ea05daf26
Anwesh43 <anweshthecool0@gmail.com>
2020-01-27 08:55:54 +0530

    adding static create method

--- sidefillsteprectview/src/main/java/com/anwesh/uiprojects/sidefillsteprectview/SideFillStepRectVew.kt
+++ sidefillsteprectview/src/main/java/com/anwesh/uiprojects/sidefillsteprectview/SideFillStepRectVew.kt
@@ -215,4 +215,13 @@ class SideFillStepRectView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : SideFillStepRectView {
+            val view : SideFillStepRectView = SideFillStepRectView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
